5f0e61e3fe7fd865ffdd9dbf7f51cb56e01722cc
Solehzoda <salikhovajillo@gmail.com>
2024-12-01 14:29:51 +0500

    первый коммит

---
+++ dz2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace dz2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе числ: ");int num2 = int.Parse(Console.ReadLine());
+            int count = 0;
+            while (num1 >= num2)
+            {
+                num1 -= num2;
+                count++; 
+            }
+            Console.WriteLine(count);
+
+        }
+    }
+}
